83b2b33117d3b7c2c94e488e4d0293a464f237d0
Gustavo Penha <gustavopenha@wlan-145-94-212-84.wlan.tudelft.nl>
2020-03-04 14:21:22 +0100

    Initial baselines for Recommendation (POP and Random)
    - included code to evaluate models using IR evaluation metrics

---
+++ list_wise_reformer/eval/evaluation.py
@@ -0,0 +1,35 @@
+import pytrec_eval
+
+METRICS = {'map',
+           'recip_rank',
+           'ndcg_cut',
+           'recall'}
+
+def evaluate_models(results):
+    """
+    Calculate METRICS for each model in the results dict
+    ----
+    Input example:
+    # results = {
+    #  'model_1': {
+    #     'preds': [[1,2],[1,2]],
+    #     'labels': [[1,2],[1,2]]
+    #   }
+    #}
+    """
+
+    for model in results.keys():
+        preds = results[model]['preds']
+        labels = results[model]['labels']
+        run = {}
+        qrel = {}
+        for i, p in enumerate(preds):
+            if 'q{}'.format(i) not in run:
+                run['q{}'.format(i+1)] = {}
+                qrel['q{}'.format(i+1)] = {}
+                for j, _ in enumerate(range(len(p))):
+                    run['q{}'.format(i+1)]['d{}'.format(j+1)] = preds[i][j]
+                    qrel['q{}'.format(i + 1)]['d{}'.format(j + 1)] = labels[i][j]
+        evaluator = pytrec_eval.RelevanceEvaluator(qrel, METRICS)
+        results[model]['eval'] = evaluator.evaluate(run)
+    return results
---
+++ list_wise_reformer/models/rec_baselines.py
@@ -0,0 +1,79 @@
+from IPython import embed
+import random
+import pandas as pd
+from collections import Counter
+
+class PopularityRecommender():
+    """
+    This recommender model ranks by popularity of the items:
+    thus the predictions are independent of the user.
+    """
+    def __init__(self):
+        self.interaction_counts = Counter()
+
+    def fit(self, sessions):
+        for _, r in sessions.iterrows():
+            for item in r['query'].split(" [SEP] "):
+                self.interaction_counts[item] +=1
+
+    def predict(self, sessions, doc_pred_columns):
+        preds = []
+        for idx, r in sessions.iterrows():
+            user_preds = []
+            for column in doc_pred_columns:
+                user_preds.append(self.interaction_counts[r[column]])
+            preds.append(user_preds)
+
+        return preds
+
+class RandomRecommender():
+    # Prediction is a uniform between 0 and 1
+    def __init__(self):
+        pass
+
+    def fit(self, sessions):
+        pass
+
+    def predict(self,sessions, doc_pred_columns):
+        preds = []
+        for _, _ in sessions.iterrows():
+            user_preds = []
+            for _ in doc_pred_columns:
+                user_preds.append(random.uniform(0,1))
+            preds.append(user_preds)
+        return preds
+
+class SASRec():
+    # TODO: IMPLEMENT
+    def __init__(self):
+        pass
+
+class BERT4Rec():
+    # TODO: IMPLEMENT
+    def __init__(self):
+        pass
+
+def test_popularity_recommender():
+    train_data = [["Lord of the Rings: The Two Towers The (2002) [SEP] "+
+                  "Back to the Future Part II (1989) [SEP] "+
+                  "Gattaca (1997)"],
+                  ["Lord of the Rings: The Two Towers The (2002) [SEP] " +
+                   "Gattaca (1997)"],
+                  ["Lord of the Rings: The Two Towers The (2002)"]]
+    test_data = [["Forrest Gump (1994) [SEP] " +
+                  "Silence of the Lambs, The (1991) [SEP] " +
+                  "Back to the Future (1985) [SEP] " +
+                  "Toy Story (1995)",
+                  "Lord of the Rings: The Two Towers The (2002)",
+                  "Back to the Future Part II (1989)",
+                  "Gattaca (1997)"]]
+    train_data = pd.DataFrame(train_data, columns=["query"])
+    test_data = pd.DataFrame(test_data, columns=["query",
+                                                 "candidate_doc_0",
+                                                 "candidate_doc_1",
+                                                 "candidate_doc_2"])
+
+    pop = PopularityRecommender()
+    pop.fit(train_data)
+    predictions = pop.predict(test_data, test_data.columns[1:])
+    assert predictions == [[3,1,2]]
